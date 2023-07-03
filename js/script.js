f416ceb1b51402b76c69f6e6be1118c63f01ef6c
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-07-03 21:43:43 +0300

    adding logic

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = `http://www.boredapi.com/api/activity?type=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+
+        document.querySelector('h2').innerText = data.activity
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
